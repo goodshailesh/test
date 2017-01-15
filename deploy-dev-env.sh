ebee043babe92424f34bc936189a0d02c69e9599
Soni Yadav <sy05dec@gmail.com>
2017-01-16 03:30:35 +1300

    Deploy Script - Passing SHA and IPADDR as argauments for calling from Jenkins

--- deploy-dev-env.sh
+++ deploy-dev-env.sh
@@ -2,10 +2,12 @@
 
 # ENVIRONEMENT VARIABLES
 #=========================
-export SHA=`git log --oneline | cut -d ' ' -f 1 | head -n 1`
+#export SHA=`git log --oneline | cut -d ' ' -f 1 | head -n 1`
+export SHA=$1
 
 # DOCKER SERVER'S IP ADDRESS
-export XIPADDR=`ifconfig | grep -A 1 'enp0s8' | tail -n 1 | tr -s ' ' ' ' | cut -d ' ' -f 3`
+#export XIPADDR=`ifconfig | grep -A 1 'enp0s8' | tail -n 1 | tr -s ' ' ' ' | cut -d ' ' -f 3`
+export XIPADDR=$2
 
 # RANDOM PORT based on last 5 digits of current epoch 
 export XPORT=`date +%s | sed 's/[[:digit:]]\{5\}\([[:digit:]]\{5\}\).*/\1/'`
